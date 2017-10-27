383cdef76e3a9d963ce7b210b740776363b50885
Joseph Cheung <josephz2314@hotmail.com>
2017-10-27 11:45:17 +0800

    make entry conciser

--- App.js
+++ App.js
@@ -1,49 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Text, View } from 'react-native'
-import { StackNavigator } from 'react-navigation'
 import { MyStatusBar } from './components'
-import { DeckDetail, AddCard, Quiz, Home } from './screens'
-import { white } from './utils/consts'
+import MainNavigator from './screens/MainNavigator'
 import { setLocalNotification } from './utils/helpers'
 import styles from './utils/styles'
 import store from './store'
-
-const MainNavigator = StackNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      header: null
-    }
-  },
-  DeckDetail: {
-    path: 'deckDetail/:id',
-    screen: DeckDetail,
-    navigationOptions: ({navigation}) => ({
-      headerTintColor: white,
-      headerStyle: styles.header,
-      title: navigation.state.params.title
-    })
-  },
-  AddCard: {
-    path: 'addCard/:id',
-    screen: AddCard,
-    navigationOptions: {
-      headerTintColor: white,
-      headerStyle: styles.header,
-      title: 'Add Card'
-    }
-  },
-  Quiz: {
-    path: 'quiz/:id',
-    screen: Quiz,
-    navigationOptions: {
-      headerTintColor: white,
-      headerStyle: styles.header,
-      title: 'Quiz'
-    }
-  }
-})
 
 export default class App extends React.Component {
   componentDidMount() {
--- screens/index.js
+++
@@ -1,4 +0,0 @@
-export { default as DeckDetail } from './DeckDetail'
-export { default as AddCard } from './AddCard'
-export { default as Quiz } from './Quiz'
-export { default as Home } from './Home'
